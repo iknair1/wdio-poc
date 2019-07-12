b176c9c415ef830a5670e7776f031a5697fa97b1
iknair1 <iknair1@gmail.com>
2019-07-12 11:14:00 +0100

    Update login.js

--- src/steps/login.js
+++ src/steps/login.js
@@ -2,20 +2,23 @@ import loginPage from '../../model/pages/login.page';
 
 function employerLogin() {
     const TRANSITION_TIMEOUT = 150000;
+    
     this.Given(/^I load the ER hub/, function() {
       loginPage.openERHub();
     });
 
     this.When(/^ I provide '(.*)' and '(.*)'/, function(username, password) {
-    loginPage.username().setValue(username);
-    loginPage.password().setValue(password);
-    loginPage.submit().click();  
+        loginPage.username().setValue(username);
+        loginPage.password().setValue(password);
+        loginPage.submit().click();  
+    });
     
-    expect(loginPage.isDashboardLoaded(TRANSITION_TIMEOUT)).to.equal(
-      true,
-      'Error in loading employer dashboard'
-    );
-  });
+    this.Then(/^ I am on the homepage'/, () {
+        expect(loginPage.isDashboardLoaded(TRANSITION_TIMEOUT)).to.equal(
+          true,
+          'Error in loading employer dashboard'
+    });
+
 }
 
 export default employerLogin;
